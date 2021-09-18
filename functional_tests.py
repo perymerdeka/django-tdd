a8d23bedb70a7234079f0c9df0d6d4dcfd7662c4
perymerdeka <tkjasikk@gmail.com>
2021-09-18 09:15:18 +0700

    Refactor functional_test.py

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,10 @@
+import time
 import os
 import unittest
 from selenium import webdriver
+from selenium.webdriver.remote.webelement import WebElement
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -30,8 +33,34 @@ class NewVisitorTest(unittest.TestCase):
         url: str = 'http://127.0.0.1:8000/'
         self.driver.get(url=url)
 
-        self.assertIn('Todo', self.driver.title)
+        self.assertIn('To-Do', self.driver.title)
         self.fail('test Finished')
+
+    def test_input_page(self):
+        url: str = 'http://127.0.0.1:8000/'
+        self.driver.get(url=url)
+        self.assertIn('To-Do', self.driver.title)
+        headers: str = self.driver.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', headers)
+
+        # test input
+        inputbox: WebElement = self.driver.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+
+        time.sleep(1)
+
+        table: WebElement = self.driver.find_element_by_id('id_list_table')
+        rows: list = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1. peacock feathers' for row in rows)
+        )
+
+        self.fail('Finish the test!')
 
 
 if __name__ == '__main__':
